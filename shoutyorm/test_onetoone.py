25e6db7861932f04d724cd736ae30fe7e573fa09
Keryn Knight <keryn@kerynknight.com>
2022-05-28 19:36:47 +0100

    chore(tests): Adding reverse onetoone behaviour.

--- shoutyorm/test_onetoone.py
+++ shoutyorm/test_onetoone.py
@@ -16,7 +16,7 @@ class ForwardOneToOneDescriptorTestCase(TestCase):
                 CassetteSideB,
                 on_delete=models.CASCADE,
                 db_column="yay_side_b",
-                related_name="woo_side_b",
+                related_name="woo_side_a",
             )
 
         try:
@@ -57,6 +57,7 @@ class ForwardOneToOneDescriptorTestCase(TestCase):
 
         with self.assertNumQueries(2):
             (side_a,) = self.CassetteSideA.objects.prefetch_related("side_b").all()
+        with self.assertNumQueries(0):
             self.assertEqual(side_a.side_b.title, "Side B!")
 
     def test_forward_one_to_one_select_relatd(self):
@@ -66,4 +67,77 @@ class ForwardOneToOneDescriptorTestCase(TestCase):
         )
         with self.assertNumQueries(1):
             (side_a,) = self.CassetteSideA.objects.select_related("side_b").all()
+        with self.assertNumQueries(0):
             self.assertEqual(side_a.side_b.title, "Side B!")
+
+
+class ReverseOneToOneDescriptorTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # type: () -> None
+        class RewindingCassetteSideB(models.Model):
+            title = models.CharField(max_length=100)
+
+        class RewindingCassetteSideA(models.Model):
+            title = models.CharField(max_length=100)
+            side_b = models.OneToOneField(
+                RewindingCassetteSideB,
+                on_delete=models.CASCADE,
+                db_column="yay_side_b",
+                related_name="woo_side_a",
+            )
+
+        try:
+            with connection.schema_editor() as editor:
+                editor.create_model(RewindingCassetteSideB)
+                editor.create_model(RewindingCassetteSideA)
+        except DatabaseError as exc:
+            raise cls.failureException("Unable to create the table (%s)" % exc)
+
+        cls.CassetteSideB = RewindingCassetteSideB
+        cls.CassetteSideA = RewindingCassetteSideA
+        super().setUpClass()
+
+    def test_reverse_one_to_one_not_selected(self):
+        """myobject.myrelation is a OneToOneField which has not been fetched"""
+        self.CassetteSideA.objects.create(
+            title="Side A!", side_b=self.CassetteSideB.objects.create(title="Side B!")
+        )
+        with self.assertNumQueries(1):
+            (side_b,) = self.CassetteSideB.objects.all()
+            with self.assertNumQueries(0):
+                self.assertEqual(side_b.title, "Side B!")
+                side_b.pk
+                with self.assertRaisesMessage(
+                    MissingOneToOneField,
+                    "Access to `CassetteSideA.side_b` was prevented.\n"
+                    "If you only need access to the column identifier, use `CassetteSideA.side_b_id` instead.\n"
+                    "To fetch the `CassetteSideB` object, add `prefetch_related('side_b')` or `select_related('side_b')` to the query where `CassetteSideA` objects are selected.",
+                ):
+                    side_b.woo_side_a
+
+    def test_reverse_one_to_one_prefetch_related(self):
+        """myobject.myrelation is the other end of a OneToOneField which has been prefetched"""
+        self.CassetteSideA.objects.create(
+            title="Side A!", side_b=self.CassetteSideB.objects.create(title="Side B!")
+        )
+
+        with self.assertNumQueries(2):
+            (side_b,) = self.CassetteSideB.objects.prefetch_related("woo_side_a").all()
+        with self.assertNumQueries(0):
+            self.assertEqual(side_b.woo_side_a.title, "Side A!")
+            side_b.pk
+            side_b.woo_side_a.pk
+
+    def test_reverse_one_to_one_select_relatd(self):
+        """myobject.myrelation is the other end of a OneToOneField which has been joined"""
+        self.CassetteSideA.objects.create(
+            title="Side A!", side_b=self.CassetteSideB.objects.create(title="Side B!")
+        )
+
+        with self.assertNumQueries(1):
+            (side_b,) = self.CassetteSideB.objects.select_related("woo_side_a").all()
+        with self.assertNumQueries(0):
+            self.assertEqual(side_b.woo_side_a.title, "Side A!")
+            side_b.pk
+            side_b.woo_side_a.pk
